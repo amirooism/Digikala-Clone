340edfdf6bcf4312952231258a9cfee830fdd323
amiro hatami <amiroism@gmail.com>
2025-02-15 19:22:07 +0330

    Ready app for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import Header from "./components/Header";
-import Product from "./components/product";
-import Example from "./components/Example";
 export default function App() {
   return (
     <>
     <Header/>
-    {/* <Product/>  */}
-    {/* <Example/> */}
+    
     </>
     
     
