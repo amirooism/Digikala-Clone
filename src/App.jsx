1e14c524147f17f74050b1660db5c2db375d992c
amirooism <119577370+amirooism@users.noreply.github.com>
2025-02-24 18:55:19 +0330

    fix for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header";
 import Product from "./components/product";
-import ProductItem from "./components/ProductItem";
 export default function App() {
   return (
     <>
